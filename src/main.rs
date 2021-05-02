5951e0d3afd02f6a29f8358d52199e8da008956c
TheMode <themode@outlook.fr>
2021-05-02 04:24:08 +0200

    Less hardcoding, error handling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io::AsyncWriteExt;
 use tokio::net::{TcpListener, TcpStream};
+use tokio::io;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut listener = TcpListener::bind("127.0.0.1:25566").await?;
+    let proxy_address = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 25566);
+    let server_address = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 25565);
+
+    let listener = TcpListener::bind(proxy_address).await?;
 
     loop {
         let (mut client_socket, _) = listener.accept().await?;
@@ -13,50 +17,45 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         tokio::spawn(async move {
 
             // Connect to minecraft server
-            let address = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 25565);
-            let mut server_socket = match TcpStream::connect(address).await {
+            let mut server_socket = match TcpStream::connect(server_address).await {
                 Ok(stream) => stream,
-                Err(error) => panic!("Error while connecting to server"),
+                Err(error) => panic!("Error while connecting to server {}", error),
             };
 
-            let mut buf = [0; 1024];
+            let mut buf = [0; 65535];
 
             // In a loop, read data from the socket and write the data back.
             loop {
                 // Read client
-                let client_size = match client_socket.try_read(&mut buf) {
+                match client_socket.try_read(&mut buf) {
                     Ok(n) if n == 0 => return,
-                    Ok(n) => n,
-                    _ => 0,
+                    Ok(n) => {
+                        if let Err(e) = server_socket.write_all(&buf[0..n]).await {
+                            eprintln!("failed to write to socket; err = {:?}", e);
+                            return;
+                        }
+                    }
+                    Err(ref e) if e.kind() != io::ErrorKind::WouldBlock => {
+                        println!("error {}",e);
+                    }
+                    _=>(),
                 };
-
-                // Send to server
-                write(&mut server_socket, &buf[0..client_size]).await;
 
                 // Read server
-                let server_size = match server_socket.try_read(&mut buf) {
+                match server_socket.try_read(&mut buf) {
                     Ok(n) if n == 0 => return,
-                    Ok(n) => n,
-                    _ => 0,
+                    Ok(n) => {
+                        if let Err(e) = client_socket.write_all(&buf[0..n]).await {
+                            eprintln!("failed to write to socket; err = {:?}", e);
+                            return;
+                        }
+                    }
+                    Err(ref e) if e.kind() != io::ErrorKind::WouldBlock => {
+                        println!("error {}",e);
+                    }
+                    _=>(),
                 };
-
-                // Send to client
-                write(&mut client_socket, &buf[0..server_size]).await;
             }
         });
-    }
-}
-
-async fn write(socket: &mut (impl AsyncWriteExt + Unpin), buffer: &[u8]) {
-    if let Err(e) = socket.write_all(buffer).await {
-        eprintln!("failed to write to socket; err = {:?}", e);
-        return;
-    }
-}
-
-async fn test(stream: TcpStream) {
-    match stream.peer_addr() {
-        Ok(address) => println!("test {}", address),
-        Err(error) => println!("error {}", error),
     }
 }
